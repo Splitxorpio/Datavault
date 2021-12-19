97353dcda5408f9c25a5c3a4c1e1745d2323f8fd
Arnav Pandey <73331288+Splitxorpio@users.noreply.github.com>
2021-12-19 12:55:12 -0800

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    MYSQL_HOST = 'localhost'
-    MYSQL_USER = 'root'
-    MYSQL_PASSWORD = ''
-    MYSQL_DB = 'datavault'
